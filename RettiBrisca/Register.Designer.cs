c72f9f91d44dc33deda26c91e050edfcaada4169
Oprea-Mihai-30127 <mihairobert05@yahoo.com>
2023-04-29 19:36:25 +0300

    Added Register function - using MySQL SSMS

--- RettiBrisca/Register.Designer.cs
+++ RettiBrisca/Register.Designer.cs
@@ -46,95 +46,107 @@
             // 
             // txtRegisterTitle
             // 
-            this.txtRegisterTitle.Location = new System.Drawing.Point(359, 53);
+            this.txtRegisterTitle.Location = new System.Drawing.Point(479, 65);
+            this.txtRegisterTitle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtRegisterTitle.Name = "txtRegisterTitle";
             this.txtRegisterTitle.ReadOnly = true;
-            this.txtRegisterTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtRegisterTitle.Size = new System.Drawing.Size(132, 22);
             this.txtRegisterTitle.TabIndex = 0;
             this.txtRegisterTitle.Text = "Register";
             // 
             // lblUsernameRegister
             // 
             this.lblUsernameRegister.AutoSize = true;
-            this.lblUsernameRegister.Location = new System.Drawing.Point(334, 124);
+            this.lblUsernameRegister.Location = new System.Drawing.Point(445, 153);
+            this.lblUsernameRegister.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblUsernameRegister.Name = "lblUsernameRegister";
-            this.lblUsernameRegister.Size = new System.Drawing.Size(55, 13);
+            this.lblUsernameRegister.Size = new System.Drawing.Size(73, 17);
             this.lblUsernameRegister.TabIndex = 2;
             this.lblUsernameRegister.Text = "Username";
             // 
             // lblPasswordRegister
             // 
             this.lblPasswordRegister.AutoSize = true;
-            this.lblPasswordRegister.Location = new System.Drawing.Point(334, 183);
+            this.lblPasswordRegister.Location = new System.Drawing.Point(445, 225);
+            this.lblPasswordRegister.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPasswordRegister.Name = "lblPasswordRegister";
-            this.lblPasswordRegister.Size = new System.Drawing.Size(53, 13);
+            this.lblPasswordRegister.Size = new System.Drawing.Size(69, 17);
             this.lblPasswordRegister.TabIndex = 3;
             this.lblPasswordRegister.Text = "Password";
             // 
             // txtPhoneRegister
             // 
             this.txtPhoneRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPhoneRegister.Location = new System.Drawing.Point(337, 310);
+            this.txtPhoneRegister.Location = new System.Drawing.Point(449, 382);
+            this.txtPhoneRegister.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPhoneRegister.Name = "txtPhoneRegister";
-            this.txtPhoneRegister.Size = new System.Drawing.Size(150, 26);
+            this.txtPhoneRegister.Size = new System.Drawing.Size(199, 30);
             this.txtPhoneRegister.TabIndex = 6;
             // 
             // lblFullNameRegister
             // 
             this.lblFullNameRegister.AutoSize = true;
-            this.lblFullNameRegister.Location = new System.Drawing.Point(334, 240);
+            this.lblFullNameRegister.Location = new System.Drawing.Point(445, 295);
+            this.lblFullNameRegister.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFullNameRegister.Name = "lblFullNameRegister";
-            this.lblFullNameRegister.Size = new System.Drawing.Size(54, 13);
+            this.lblFullNameRegister.Size = new System.Drawing.Size(71, 17);
             this.lblFullNameRegister.TabIndex = 7;
             this.lblFullNameRegister.Text = "Full Name";
             // 
             // lblPhoneRegister
             // 
             this.lblPhoneRegister.AutoSize = true;
-            this.lblPhoneRegister.Location = new System.Drawing.Point(334, 294);
+            this.lblPhoneRegister.Location = new System.Drawing.Point(445, 362);
+            this.lblPhoneRegister.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPhoneRegister.Name = "lblPhoneRegister";
-            this.lblPhoneRegister.Size = new System.Drawing.Size(78, 13);
+            this.lblPhoneRegister.Size = new System.Drawing.Size(103, 17);
             this.lblPhoneRegister.TabIndex = 8;
             this.lblPhoneRegister.Text = "Phone Number";
             // 
             // txtFullNameRegister
             // 
             this.txtFullNameRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFullNameRegister.Location = new System.Drawing.Point(337, 256);
+            this.txtFullNameRegister.Location = new System.Drawing.Point(449, 315);
+            this.txtFullNameRegister.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtFullNameRegister.Name = "txtFullNameRegister";
-            this.txtFullNameRegister.Size = new System.Drawing.Size(150, 26);
+            this.txtFullNameRegister.Size = new System.Drawing.Size(199, 30);
             this.txtFullNameRegister.TabIndex = 9;
             // 
             // txtPasswordRegister
             // 
             this.txtPasswordRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPasswordRegister.Location = new System.Drawing.Point(337, 199);
+            this.txtPasswordRegister.Location = new System.Drawing.Point(449, 245);
+            this.txtPasswordRegister.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPasswordRegister.Name = "txtPasswordRegister";
-            this.txtPasswordRegister.Size = new System.Drawing.Size(150, 26);
+            this.txtPasswordRegister.Size = new System.Drawing.Size(199, 30);
             this.txtPasswordRegister.TabIndex = 10;
             // 
             // textUsernameRegister
             // 
             this.textUsernameRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textUsernameRegister.Location = new System.Drawing.Point(337, 140);
+            this.textUsernameRegister.Location = new System.Drawing.Point(449, 172);
+            this.textUsernameRegister.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textUsernameRegister.Name = "textUsernameRegister";
-            this.textUsernameRegister.Size = new System.Drawing.Size(150, 26);
+            this.textUsernameRegister.Size = new System.Drawing.Size(199, 30);
             this.textUsernameRegister.TabIndex = 11;
             // 
             // btnAddUser
             // 
-            this.btnAddUser.Location = new System.Drawing.Point(372, 363);
+            this.btnAddUser.Location = new System.Drawing.Point(496, 447);
+            this.btnAddUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAddUser.Name = "btnAddUser";
-            this.btnAddUser.Size = new System.Drawing.Size(75, 23);
+            this.btnAddUser.Size = new System.Drawing.Size(100, 28);
             this.btnAddUser.TabIndex = 12;
             this.btnAddUser.Text = "Register";
             this.btnAddUser.UseVisualStyleBackColor = true;
+            this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(713, 396);
+            this.btnExit.Location = new System.Drawing.Point(951, 487);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Size = new System.Drawing.Size(100, 28);
             this.btnExit.TabIndex = 13;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -142,9 +154,10 @@
             // 
             // btnGoHome
             // 
-            this.btnGoHome.Location = new System.Drawing.Point(12, 396);
+            this.btnGoHome.Location = new System.Drawing.Point(16, 487);
+            this.btnGoHome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnGoHome.Name = "btnGoHome";
-            this.btnGoHome.Size = new System.Drawing.Size(75, 23);
+            this.btnGoHome.Size = new System.Drawing.Size(100, 28);
             this.btnGoHome.TabIndex = 14;
             this.btnGoHome.Text = "Home";
             this.btnGoHome.UseVisualStyleBackColor = true;
@@ -153,17 +166,19 @@
             // lblExistAccount
             // 
             this.lblExistAccount.AutoSize = true;
-            this.lblExistAccount.Location = new System.Drawing.Point(334, 401);
+            this.lblExistAccount.Location = new System.Drawing.Point(445, 494);
+            this.lblExistAccount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblExistAccount.Name = "lblExistAccount";
-            this.lblExistAccount.Size = new System.Drawing.Size(97, 13);
+            this.lblExistAccount.Size = new System.Drawing.Size(131, 17);
             this.lblExistAccount.TabIndex = 15;
             this.lblExistAccount.Text = "Already a member?";
             // 
             // btnGoLogIn
             // 
-            this.btnGoLogIn.Location = new System.Drawing.Point(437, 396);
+            this.btnGoLogIn.Location = new System.Drawing.Point(583, 487);
+            this.btnGoLogIn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnGoLogIn.Name = "btnGoLogIn";
-            this.btnGoLogIn.Size = new System.Drawing.Size(75, 23);
+            this.btnGoLogIn.Size = new System.Drawing.Size(100, 28);
             this.btnGoLogIn.TabIndex = 16;
             this.btnGoLogIn.Text = "Log In";
             this.btnGoLogIn.UseVisualStyleBackColor = true;
@@ -171,9 +186,9 @@
             // 
             // Register
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.btnGoLogIn);
             this.Controls.Add(this.lblExistAccount);
             this.Controls.Add(this.btnGoHome);
@@ -188,6 +203,7 @@
             this.Controls.Add(this.lblPasswordRegister);
             this.Controls.Add(this.lblUsernameRegister);
             this.Controls.Add(this.txtRegisterTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Register";
             this.Text = "Register";
             this.ResumeLayout(false);
--- RettiBrisca/Register.cs
+++ RettiBrisca/Register.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace RettiBrisca
 {
     public partial class Register : Form
     {
+        private SqlConnection DbConnection = new SqlConnection("Data Source=" +
+                "DESKTOP-GTQ68AU\\SQLEXPRESS01;Initial Catalog=BDD;Integrated Security=True");
+
         public Register()
         {
             InitializeComponent();
@@ -34,6 +39,97 @@ namespace RettiBrisca
             LogIn logIn = new LogIn();
             logIn.Show();
             this.Hide();
+        }
+
+        private void btnAddUser_Click(object sender, EventArgs e)
+        {
+            string username = textUsernameRegister.Text;
+            string password = txtPasswordRegister.Text;
+            string fullName = txtFullNameRegister.Text;
+            string phoneNumber = txtPhoneRegister.Text;
+
+            if (UsernameExists(username))
+            {
+                MessageBox.Show("Exista deja un utilizator cu acest username. Te rugam alege alt username pentru a proceda mai departe.");
+            }
+            else
+            {
+                if (username.Length < 5)
+                {
+                    MessageBox.Show("Username trebuie sa contina minim 6 caractere.");
+                }
+                else if (password.Length < 7)
+                {
+                    MessageBox.Show("Password trebuie sa contina minim 8 caractere.");
+                }
+                else if (fullName.Length < 5)
+                {
+                    MessageBox.Show("Full Name trebuie sa contina minim 6 caractere.");
+                }
+                else if (phoneNumber.Length != 10)
+                {
+                    MessageBox.Show("Numarul de telefon nu este de 10 cifre, corecteaza inainte de a proceda mai departe.");
+                }
+                else
+                {
+                    try
+                    {
+                        DbConnection.Open();
+                        SqlCommand comanda = new SqlCommand("INSERT INTO Client (Username, Password, PhoneNumber, FullName) VALUES(@username, @password, @phoneNumber, @fullName)", DbConnection);
+                        comanda.Parameters.AddWithValue("@Username", username);
+                        comanda.Parameters.AddWithValue("@Password", password);
+                        comanda.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+                        comanda.Parameters.AddWithValue("@FullName", fullName);
+
+                        comanda.ExecuteNonQuery();
+
+                        MessageBox.Show("Inregistrare facuta cu succes!");
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally
+                    {
+                        DbConnection.Close();
+                    }
+                }
+            }
+        }
+
+        private Boolean UsernameExists(string username)
+        {
+            int ok = 1;
+            try
+            {
+                DbConnection.Open();
+                using(SqlCommand comanda = new SqlCommand("SELECT COUNT(*) FROM Client WHERE Username = @username", DbConnection))
+                {
+                    comanda.Parameters.AddWithValue("@username", username);
+                    int count = (int)comanda.ExecuteScalar();
+                    if(count > 0)
+                    {
+                        ok = 0;
+                    }
+                    
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                DbConnection.Close();
+            }
+            if(ok == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }   
         }
     }
 }
